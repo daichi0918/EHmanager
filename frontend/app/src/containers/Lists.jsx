6ac54c535e1de7dd2b51aa7819fe6b9652b9f6f5
daichi0918 <kayasugadaichi@icloud.com>
2022-08-13 10:07:23 +0900

    リストAPIのデータをreducerを通して画面に表示する

--- frontend/app/src/containers/Lists.jsx
+++ frontend/app/src/containers/Lists.jsx
@@ -1,22 +1,52 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useReducer } from 'react';
+
+import {
+  initialState as listsInitialState,
+  listsActionTyps,
+  listsReducer,
+} from '../reducers/lists';
 
 // apis
 import { fetchLists } from '../apis/lists';
+
+// constants
+import { REQUEST_STATE } from '../constants';
 
 export const Lists = ({
   match
 }) => {
 
+  const [listsState, dispatch] = useReducer(listsReducer, listsInitialState);
+
   useEffect(() => {
+    dispatch({ type: listsActionTyps.FETCHING });
     fetchLists(match.params.usersId)
-      .then((data) =>
-        console.log(data)
-      )
+      .then((data) => {
+        dispatch({
+          type: listsActionTyps.FETCH_SUCCESS,
+          payload: {
+            lists: data.lists
+          }
+        });
+      })
   }, [])
 
   return (
     <Fragment>
-      リスト一覧
+      {
+        listsState.fetchState === REQUEST_STATE.LOADING ?
+          <Fragment>
+            <p>
+              ロード中...
+                  </p>
+          </Fragment>
+          :
+          listsState.buyList.map(list =>
+            <div key={list.id}>
+              {list.name}
+            </div>
+          )
+      }
     </Fragment>
   )
 }
